34ce8658bbcffeea3086cd0c9f4294b72021baae
MattWagner28 <matthewwagner@outlook.com>
2023-04-18 21:25:12 -0400

    added add an employee function yayyy

--- index.js
+++ index.js
@@ -64,6 +64,37 @@ const newRolePrompt = [
   },
 ];
 
+const newEmployeePrompt = [
+    {
+        type: "input",
+        message: "Please enter employee's first name.",
+        name: "firstName",
+    },
+    {
+        type: "input",
+        message: "Please enter employee's last name.",
+        name: "lastName",
+    },
+    {
+        type: "list",
+        message: "Please select a role for this employee.",
+        choices: async function () {
+          const roleNames = await getRoleArray();
+          return roleNames;
+        },
+        name: "role_id",
+    },
+    {
+        type: "list",
+        message: "Please select a manager for this employee.",
+        choices: async function () {
+            const mgrNames = await getManagerArray();
+            return mgrNames;
+        },
+        name: "manager_id",
+    }
+]
+
 // Query displaying departments table
 const getDeptTable = () => {
   connection.query("SELECT * FROM DEPARTMENT", function (err, results, fields) {
@@ -118,6 +149,72 @@ const getRoleTable = () => {
   });
 };
 
+// Retrieves up-to-date array of role names for whenever user needs to add a role in a query
+const getRoleArray = () => {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT title FROM role", function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          const roleNames = results.map(result => result.title);
+          resolve(roleNames);
+        }
+      });
+    });
+  };
+
+  //Converts selected role from query to the ID to then add to a table
+const getRoleID = (role_id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `SELECT id FROM role WHERE title = '${role_id}';`,
+        function (err, results, fields) {
+          if (err) {
+            reject(err);
+          } else {
+            const id = parseInt(results[0].id);
+            resolve(id);
+          }
+        }
+      );
+    });
+  };
+
+  
+
+// Retrieves up-to-date array of Manager names for whenever user needs to add a department in a query
+const getManagerArray = () => {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          const mgrNames = results.map(result => `${result.first_name} ${result.last_name}`);
+          resolve(mgrNames);
+        }
+      });
+    });
+  };
+
+  //Converts selected manager from query to the ID to then add to a table
+const getManagerID = (manager_id) => {
+    return new Promise((resolve, reject) => {
+      const names = manager_id.split(' ');
+      mgrLastName = names[1];
+      connection.query(
+        `SELECT id FROM employee WHERE last_name='${mgrLastName}'`,
+        function (err, results, fields) {
+          if (err) {
+            reject(err);
+          } else {
+            const id = parseInt(results[0].id);
+            resolve(id);
+          }
+        }
+      );
+    });
+  };
+
 // Query displaying role table
 const getEmployeeTable = () => {
   connection.query("SELECT * FROM EMPLOYEE", function (err, results, fields) {
@@ -149,6 +246,16 @@ const addRole = (role, salary, department_id) => {
   );
 };
 
+const addEmployee = (firstName, lastName, role_id, manager_id) => {
+    connection.query(
+      `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', ${role_id}, ${manager_id});`,
+      function (err, results) {
+        if (err) throw err;
+        console.log("New employee added successfully.");
+      }
+    );
+  };
+
 // TODO: Create a function to initialize app
 // Function to initialize app
 async function init() {
@@ -165,6 +272,11 @@ async function init() {
   } else if (answers.action === "Add a Role") {
     const roleAnswers = await inquirer.prompt(newRolePrompt);
     await addRole(roleAnswers.title, roleAnswers.salary, await getDeptID(roleAnswers.department_id));
+  } else if (answers.action === "Add an Employee") {
+    // await getManagerArray();
+    const empAnswers = await inquirer.prompt(newEmployeePrompt);
+    // await getManagerID(empAnswers.manager_id);
+    await addEmployee(empAnswers.firstName, empAnswers.lastName, await getRoleID(empAnswers.role_id), await getManagerID(empAnswers.manager_id));
   }
 }
 
