0ba3cb81990bdeae9718491af86f8ee5e49d3d0c
MattWagner28 <matthewwagner@outlook.com>
2023-04-19 01:37:28 -0400

    finished project!

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const mainMenu = [
       "Add a Role",
       "Add an Employee",
       "Update an Employee Role",
+      "Quit",
     ],
     name: "action",
     // validate: function () {
@@ -42,6 +43,7 @@ const newDeptPrompt = [
   },
 ];
 
+//Prompts if user wants to add a new role
 const newRolePrompt = [
   {
     type: "input",
@@ -57,6 +59,7 @@ const newRolePrompt = [
     type: "list",
     message: "Please select a department for the new role.",
     choices: async function () {
+      //Provides updated list of department names
       const deptNames = await getDeptArray();
       return deptNames;
     },
@@ -64,45 +67,78 @@ const newRolePrompt = [
   },
 ];
 
+//Prompts if user wants to add a new employee
 const newEmployeePrompt = [
-    {
-        type: "input",
-        message: "Please enter employee's first name.",
-        name: "firstName",
+  {
+    type: "input",
+    message: "Please enter employee's first name.",
+    name: "firstName",
+  },
+  {
+    type: "input",
+    message: "Please enter employee's last name.",
+    name: "lastName",
+  },
+  {
+    type: "list",
+    message: "Please select a role for this employee.",
+    choices: async function () {
+          //Provides updated list of roles
+      const roleNames = await getRoleArray();
+      return roleNames;
     },
-    {
-        type: "input",
-        message: "Please enter employee's last name.",
-        name: "lastName",
+    name: "role_id",
+  },
+  {
+    type: "list",
+    message: "Please select a manager for this employee.",
+    choices: async function () {
+        //Provides updated list of managers
+      const mgrNames = await getManagerArray();
+      return mgrNames;
     },
-    {
-        type: "list",
-        message: "Please select a role for this employee.",
-        choices: async function () {
-          const roleNames = await getRoleArray();
-          return roleNames;
-        },
-        name: "role_id",
+    name: "manager_id",
+  },
+];
+
+//Prompts if user wants to update a role
+const updateRolePrompt = [
+  {
+    type: "list",
+    message: "Please select an employee.",
+    choices: async function () {
+      const employeeNames = await getEmployeeArray();
+      return employeeNames;
     },
-    {
-        type: "list",
-        message: "Please select a manager for this employee.",
-        choices: async function () {
-            const mgrNames = await getManagerArray();
-            return mgrNames;
-        },
-        name: "manager_id",
-    }
+    name: "employee_name",
+  },
+  {
+    type: "list",
+    message: "Please select a new role for this employee.",
+    choices: async function () {
+      const roleNames = await getRoleArray();
+      return roleNames;
+    },
+    name: "role_id",
+  }
 ]
+
+//Could not get this to work smoothly, but after each task, it would ask the user 
+//if they wanted to keep using the app
+const keepGoingPrompt = [
+  {
+    type: "list",
+    message: "Would you like to do something else?",
+    choices: ["Yes", "No"],
+    name: "yesOrNo",
+  },
+];
 
 // Query displaying departments table
 const getDeptTable = () => {
   connection.query("SELECT * FROM DEPARTMENT", function (err, results, fields) {
-    // console.log(results); // results contains rows returned by server
-    // console.log(fields); // fields contains extra meta data about results, if available
     let deptTable = cTable.getTable(results);
-    // console.log('DEPARTMENTS');
-    console.log(deptTable);
+    console.table(deptTable);
   });
 };
 
@@ -123,20 +159,20 @@ const getDeptArray = () => {
 
 //Converts selected department from query to the ID to then add to a table
 const getDeptID = (department_id) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        `SELECT id FROM department WHERE name = '${department_id}';`,
-        function (err, results, fields) {
-          if (err) {
-            reject(err);
-          } else {
-            const id = parseInt(results[0].id);
-            resolve(id);
-          }
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT id FROM department WHERE name = '${department_id}';`,
+      function (err, results, fields) {
+        if (err) {
+          reject(err);
+        } else {
+          const id = parseInt(results[0].id);
+          resolve(id);
         }
-      );
-    });
-  };
+      }
+    );
+  });
+};
 
 // Query displaying role table
 const getRoleTable = () => {
@@ -151,69 +187,72 @@ const getRoleTable = () => {
 
 // Retrieves up-to-date array of role names for whenever user needs to add a role in a query
 const getRoleArray = () => {
-    return new Promise((resolve, reject) => {
-      connection.query("SELECT title FROM role", function (err, results) {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT title FROM role", function (err, results) {
+      if (err) {
+        reject(err);
+      } else {
+        const roleNames = results.map((result) => result.title);
+        resolve(roleNames);
+      }
+    });
+  });
+};
+
+//Converts selected role from query to the ID to then add to a table
+const getRoleID = (role_id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT id FROM role WHERE title = '${role_id}'`,
+      function (err, results, fields) {
         if (err) {
           reject(err);
         } else {
-          const roleNames = results.map(result => result.title);
-          resolve(roleNames);
+          const id = parseInt(results[0].id);
+          resolve(id);
         }
-      });
-    });
-  };
-
-  //Converts selected role from query to the ID to then add to a table
-const getRoleID = (role_id) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        `SELECT id FROM role WHERE title = '${role_id}';`,
-        function (err, results, fields) {
-          if (err) {
-            reject(err);
-          } else {
-            const id = parseInt(results[0].id);
-            resolve(id);
-          }
-        }
-      );
-    });
-  };
-
-  
+      }
+    );
+  });
+};
 
 // Retrieves up-to-date array of Manager names for whenever user needs to add a department in a query
 const getManagerArray = () => {
-    return new Promise((resolve, reject) => {
-      connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function (err, results) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT first_name, last_name FROM employee WHERE manager_id IS NULL",
+      function (err, results) {
         if (err) {
           reject(err);
         } else {
-          const mgrNames = results.map(result => `${result.first_name} ${result.last_name}`);
+          const mgrNames = results.map(
+            (result) => `${result.first_name} ${result.last_name}`
+          );
           resolve(mgrNames);
         }
-      });
-    });
-  };
+      }
+    );
+  });
+};
 
-  //Converts selected manager from query to the ID to then add to a table
+//Converts selected manager from query to the ID to then add to a table
 const getManagerID = (manager_id) => {
-    return new Promise((resolve, reject) => {
-      const names = manager_id.split(' ');
-      mgrLastName = names[1];
-      connection.query(
-        `SELECT id FROM employee WHERE last_name='${mgrLastName}'`,
-        function (err, results, fields) {
-          if (err) {
-            reject(err);
-          } else {
-            const id = parseInt(results[0].id);
-            resolve(id);
-          }
+  return new Promise((resolve, reject) => {
+    const names = manager_id.split(" ");
+    mgrLastName = names[1];
+    connection.query(
+      `SELECT id FROM employee WHERE last_name='${mgrLastName}'`,
+      function (err, results, fields) {
+        if (err) {
+          reject(err);
+        } else {
+          const id = parseInt(results[0].id);
+          resolve(id);
         }
-      );
-    });
-  };
+      }
+    );
+  });
+};
 
 // Query displaying role table
 const getEmployeeTable = () => {
@@ -236,6 +275,25 @@ const addDept = (department) => {
   );
 };
 
+// Retrieves up-to-date array of employee names for whenever user needs to update an employee role in a query
+const getEmployeeArray = () => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT first_name, last_name FROM employee",
+      function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          const employeeNames = results.map(
+            (result) => `${result.first_name} ${result.last_name}`
+          );
+          resolve(employeeNames);
+        }
+      }
+    );
+  });
+};
+
 const addRole = (role, salary, department_id) => {
   connection.query(
     `INSERT INTO role (title, salary, department_id) VALUES ('${role}', ${salary}, ${department_id});`,
@@ -247,21 +305,45 @@ const addRole = (role, salary, department_id) => {
 };
 
 const addEmployee = (firstName, lastName, role_id, manager_id) => {
-    connection.query(
-      `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', ${role_id}, ${manager_id});`,
-      function (err, results) {
-        if (err) throw err;
-        console.log("New employee added successfully.");
-      }
-    );
-  };
+  connection.query(
+    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', ${role_id}, ${manager_id});`,
+    function (err, results) {
+      if (err) throw err;
+      console.log("New employee added successfully.");
+    }
+  );
+};
+
+//Replaces a selected employee's role
+const replaceRole = async (employee_name, role_id) => {
+  const splitNames = await employee_name.split(" ");
+  const lastName = await splitNames[1];
+  const roleID = await getRoleID(role_id);
+  connection.query(
+    `UPDATE employee SET role_id = ${roleID} WHERE last_name = '${lastName}'`,
+    function (err, results) {
+      if (err) throw err;
+      console.log("Role has been successfully updated.")
+    }
+  );
+};
+
+//Couldn't get this to work smoothly
+  // const keepGoing = async () => {
+  //   const answers = await inquirer.prompt(keepGoingPrompt);
+  //   if (answers.yesOrNo === "No" ) {
+  //     console.log("Goodbye!");
+  //   } init();
+  // };
 
 // TODO: Create a function to initialize app
-// Function to initialize app
+
+// Function to initialize app and go through the prompts depending on what the user chooses
 async function init() {
   const answers = await inquirer.prompt(mainMenu);
   if (answers.action === "View All Departments") {
     await getDeptTable();
+    // await keepGoing();
   } else if (answers.action === "View All Roles") {
     await getRoleTable();
   } else if (answers.action === "View All Employees") {
@@ -271,12 +353,27 @@ async function init() {
     await addDept(deptAnswers.department);
   } else if (answers.action === "Add a Role") {
     const roleAnswers = await inquirer.prompt(newRolePrompt);
-    await addRole(roleAnswers.title, roleAnswers.salary, await getDeptID(roleAnswers.department_id));
+    await addRole(
+      roleAnswers.title,
+      roleAnswers.salary,
+      await getDeptID(roleAnswers.department_id)
+    );
   } else if (answers.action === "Add an Employee") {
-    // await getManagerArray();
     const empAnswers = await inquirer.prompt(newEmployeePrompt);
-    // await getManagerID(empAnswers.manager_id);
-    await addEmployee(empAnswers.firstName, empAnswers.lastName, await getRoleID(empAnswers.role_id), await getManagerID(empAnswers.manager_id));
+    await addEmployee(
+      empAnswers.firstName,
+      empAnswers.lastName,
+      await getRoleID(empAnswers.role_id),
+      await getManagerID(empAnswers.manager_id)
+    );
+  } else if (answers.action === "Update an Employee Role") {
+    const updateRoleAnswers = await inquirer.prompt(updateRolePrompt);
+    await replaceRole(
+      updateRoleAnswers.employee_name,
+      updateRoleAnswers.role_id
+    );
+  } else {
+    console.log("Goodbye!")
   }
 }
 
